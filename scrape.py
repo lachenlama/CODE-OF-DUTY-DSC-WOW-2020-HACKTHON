31786d495a5d8ffed49e3347586a246d28b445b4
Swaroop <Swaroop.rajlama2001@outlook.com>
2020-12-11 19:52:35 +0530

    keys for selenium, automated search options added

--- scrape.py
+++ scrape.py
@@ -2,6 +2,7 @@
 
 import bs4
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 HEADER = {'User-Agent':
             'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36','Accept-Language':'en-US , en;q=0.5'
@@ -11,3 +12,10 @@ URL = "https://www.amazon.in/"
 driver = webdriver.Chrome('chromedriver')
 driver.implicitly_wait(10)
 driver.get(URL)
+
+
+print("What do you want?")
+search_term = str(input(": "))
+search_button = driver.find_element_by_id("twotabsearchtextbox")
+search_term.send_keys(search_term)
+search_term.send_keys(Keys.ENTER)
