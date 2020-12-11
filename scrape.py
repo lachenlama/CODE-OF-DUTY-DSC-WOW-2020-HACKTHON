89e5d9416cf32b19b3591ff9453b6cfef4da1672
Swaroop <Swaroop.rajlama2001@outlook.com>
2020-12-11 21:43:38 +0530

    function get_rating called

--- scrape.py
+++ scrape.py
@@ -7,6 +7,7 @@ from selenium.webdriver.common.keys import Keys
 from functions import get_title
 from functions import get_price_mrp
 from functions import get_price_deal_price
+from functions import get_rating
 
 HEADER = {'User-Agent':
             'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36','Accept-Language':'en-US , en;q=0.5'
@@ -37,3 +38,4 @@ for link in links_storage:
     print("Name= ", get_title(new_soup))
     print("Mrp= ", get_price_mrp(new_soup))
     print("You pay= ", get_price_deal_price(new_soup))
+    print("Rating= ", get_rating(new_soup))
