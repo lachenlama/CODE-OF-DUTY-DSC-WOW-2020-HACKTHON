3bc6d24070aa12e5eb4c815b22374307d74f0d3e
Swaroop <Swaroop.rajlama2001@outlook.com>
2020-12-11 21:26:23 +0530

    calling title, price functions

--- scrape.py
+++ scrape.py
@@ -4,6 +4,9 @@ import html5lib
 import bs4
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from functions import get_title
+from functions import get_price_mrp
+from functions import get_price_deal_price
 
 HEADER = {'User-Agent':
             'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36','Accept-Language':'en-US , en;q=0.5'
@@ -13,13 +16,11 @@ URL = "https://www.amazon.in/"
 driver = webdriver.Chrome('chromedriver')
 driver.implicitly_wait(10)
 driver.get(URL)
-
-
-print("What do you want?")
-search_term = str(input(": "))
+#Using selenium to drive the browser
 search_button = driver.find_element_by_id("twotabsearchtextbox")
-search_term.send_keys(search_term)
-search_term.send_keys(Keys.ENTER)
+search_term = str(input("What do you want?\n:"))
+search_button.send_keys(search_term)
+search_button.send_keys(Keys.ENTER)
 
 soup = bs4.BeautifulSoup(driver.page_source, "html5lib")
 
@@ -31,4 +32,8 @@ for link in links:
 for link in links_storage:
     new_webpage = driver.get(URL + link)
     new_soup = bs4.BeautifulSoup(driver.page_source, "html5lib")
-    
+
+    print()
+    print("Name= ", get_title(new_soup))
+    print("Mrp= ", get_price_mrp(new_soup))
+    print("You pay= ", get_price_deal_price(new_soup))
